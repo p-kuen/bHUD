4e337c1d04b9713ee353f58a11649cb2df01188f
Patrick Kuen <patrick.kuen11@gmail.com>
2014-05-26 20:55:50 +0200

    Added workshopupdater and workshopcreator

---
+++ workshopcreator.bat
@@ -0,0 +1,5 @@
+"../../../bin/gmad.exe" create -out "D:\Benutzer\Patrick\Dateien\Workshop\bhud.gma" -folder "D:\Benutzer\Patrick\Dateien\Server\Valve-Server\steamapps\common\GarrysModDS\garrysmod\addons\bHUD"
+
+"../../../bin/gmpublish" create -addon "D:\Benutzer\Patrick\Dateien\Workshop\bhud.gma" -icon "D:\Benutzer\Patrick\Pictures\Workshop\bhud.jpg"
+
+PAUSE
---
+++ workshopupdater.bat
@@ -0,0 +1,5 @@
+"../../../bin/gmad.exe" create -out "D:\Benutzer\Patrick\Dateien\Workshop\bhud.gma" -folder "D:\Benutzer\Patrick\Dateien\Server\Valve-Server\steamapps\common\GarrysModDS\garrysmod\addons\bHUD"
+
+"../../../bin/gmpublish" update -addon "D:\Benutzer\Patrick\Dateien\Workshop\bhud.gma" -id "175630527" -changes "Fixed a strange bug"
+
+PAUSE
