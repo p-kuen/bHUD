de8bd19e7940f8741cfb186f2e001cde639df3ad
Patcher56 <patrick.kuen11@gmail.com>
2014-06-15 00:04:45 +0200

    Updated workshopupdater.bat

--- workshopupdater.bat
+++ workshopupdater.bat
@@ -1,5 +1,5 @@
-"../../../bin/gmad.exe" create -out "D:\Benutzer\Patrick\Dateien\Workshop\bhud.gma" -folder "D:\Benutzer\Patrick\Dateien\Server\Valve-Server\steamapps\common\GarrysModDS\garrysmod\addons\bHUD"
+"../../../bin/gmad.exe" create -out "D:\Daten\Workshop\bhud.gma" -folder "D:\Daten\Server\Valve Server\steamapps\common\GarrysModDS\garrysmod\addons\bHUD"
 
-"../../../bin/gmpublish" update -addon "D:\Benutzer\Patrick\Dateien\Workshop\bhud.gma" -id "175630527" -changes "Fixed a strange bug"
+"../../../bin/gmpublish" update -addon "D:\Daten\Workshop\bhud.gma" -id "175630527" -changes "UPDATE INFOS WILL SHOW UP HERE SOON"
 
 PAUSE
