5f68a19db99688795632df63150cf345a6e77f2f
Domii894 <domii894@gmail.com>
2014-12-30 23:25:03 +0100

    improved workshopupdater

--- workshopupdater.bat
+++ workshopupdater.bat
@@ -1,5 +1,5 @@
+@echo off
+set /p changes="Changes: "
 "../../../bin/gmad.exe" create -out "D:\Daten\Workshop\bhud.gma" -folder "D:\Daten\Server\Valve Server\steamapps\common\GarrysModDS\garrysmod\addons\bHUD"
-
-"../../../bin/gmpublish" update -addon "D:\Daten\Workshop\bhud.gma" -id "175630527" -changes "UPDATE INFOS WILL SHOW UP HERE SOON"
-
+"../../../bin/gmpublish" update -addon "D:\Daten\Workshop\bhud.gma" -id "175630527" -changes "%changes%"
 PAUSE
