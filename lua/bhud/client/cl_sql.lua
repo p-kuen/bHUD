8a7d87362be2ebfeb8fa32637e9ff6a7ea7f68ae
Domii894 <domii894@gmail.com>
2015-01-25 22:23:45 +0100

    Improved cl_sql.lua

--- lua/bhud/client/cl_sql.lua
+++ lua/bhud/client/cl_sql.lua
@@ -9,20 +9,21 @@ end
 sql.Query( "CREATE TABLE IF NOT EXISTS bhud_settings( 'setting' TEXT, value TEXT )" )
 
 -- SET DEFAULT SQL-SETTINGS
-local check_sql = {}
-check_sql["drawHUD"] = true
-check_sql["drawPlayerHUD"] = true
-check_sql["player_name"] = false
-check_sql["drawHoverNames"] = true
-check_sql["drawTimeHUD"] = true
-check_sql["showday"] = false
-check_sql["drawMapHUD"] = true
-check_sql["design"] = 1
-check_sql["map_radius"] = 100
-check_sql["map_border"] = 3
-check_sql["map_left"] = ScrW() - check_sql["map_radius"] - check_sql["map_border"] - 10
-check_sql["map_top"] = ScrH() - check_sql["map_radius"] - check_sql["map_border"] - 10
-check_sql["map_tolerance"] = 200
+local check_sql = {
+	drawHUD = true,
+	drawPlayerHUD = true,
+	player_name = false,
+	drawHoverNames = true,
+	drawTimeHUD = true,
+	showday = false,
+	drawMapHUD = true,
+	design = 1,
+	map_radius = 100,
+	map_border = 3,
+	map_left = ScrW() - 103 - 10,
+	map_top = ScrH() - 103 - 10,
+	map_tolerance = 200
+}
 
 -- LOAD CUSTOM SQL-SETTINGS
 table.foreach( check_sql, function( setting, value )
@@ -33,9 +34,9 @@ table.foreach( check_sql, function( setting, value )
 
 	local val = sql.QueryValue( "SELECT value FROM bhud_settings WHERE setting = '" .. setting .. "'" )
 	if val == "true" or val == "false" then
-		cl_bHUD.Settings[setting] = tobool( val )
+		cl_bHUD.Settings[ setting ] = tobool( val )
 	else
-		cl_bHUD.Settings[setting] = tonumber( val )
+		cl_bHUD.Settings[ setting ] = tonumber( val )
 	end
 
 end )
@@ -43,13 +44,11 @@ end )
 -- PANEL
 function cl_bHUD.SettingsPanel()
 
-	local pw = 500
-	local ph = 285
-	local px = ScrW() / 2 - ( pw / 2 )
-	local py = ScrH() / 2 - ( ph / 2 )
+	local pw, ph, ch = 500, 285, 35
+	local px, py = ScrW() * 0.5 - ( pw * 0.5 ), ScrH() * 0.5 - ( ph * 0.5 )
 
 	local frm = cl_bHUD.addfrm( px, py, pw, ph )
-	local ch = 35
+
 	cl_bHUD.addlbl( frm, "General:", 10, ch )
 	ch = ch + 25
 	cl_bHUD.addchk( frm, "Enable bHUD", 10, ch, "drawHUD" )
@@ -59,7 +58,7 @@ function cl_bHUD.SettingsPanel()
 	ch = ch + 25
 	cl_bHUD.addchk( frm, "Enable Player-HUD", 10, ch, "drawPlayerHUD" )
 	ch = ch + 20
-	cl_bHUD.addsld( frm, "Design", 10, ch, 155, 1, cl_bHUD.Settings["designs"], cl_bHUD.Settings["design"], "design" )
+	cl_bHUD.addsld( frm, "Design", 10, ch, 155, 1, cl_bHUD.Settings.designs, cl_bHUD.Settings.design, "design" )
 	ch = ch + 29
 	cl_bHUD.addchk( frm, "Show Player Name", 10, ch, "player_name" )
 	ch = ch + 20
@@ -77,13 +76,13 @@ function cl_bHUD.SettingsPanel()
 	ch = ch + 25
 	cl_bHUD.addchk( frm, "Show Minimap", pw / 2 + 13, ch, "drawMapHUD" )
 	ch = ch + 25
-	cl_bHUD.addsld( frm, "Radius", pw / 2 + 13, ch, 155, 50, 150, cl_bHUD.Settings["map_radius"], "map_radius" )
+	cl_bHUD.addsld( frm, "Radius", pw / 2 + 13, ch, 155, 50, 150, cl_bHUD.Settings.map_radius, "map_radius" )
 	ch = ch + 25
-	cl_bHUD.addsld( frm, "Border", pw / 2 + 13, ch, 155, 0, 15, cl_bHUD.Settings["map_border"], "map_border" )
+	cl_bHUD.addsld( frm, "Border", pw / 2 + 13, ch, 155, 0, 15, cl_bHUD.Settings.map_border, "map_border" )
 	ch = ch + 25
-	cl_bHUD.addsld( frm, "X-Position", pw / 2 + 13, ch, 155, 10 + cl_bHUD.Settings["map_radius"] + cl_bHUD.Settings["map_border"], ScrW() - cl_bHUD.Settings["map_radius"] - 10 - cl_bHUD.Settings["map_border"], cl_bHUD.Settings["map_left"], "map_left" )
+	cl_bHUD.addsld( frm, "X-Position", pw / 2 + 13, ch, 155, 10 + cl_bHUD.Settings.map_radius + cl_bHUD.Settings.map_border, ScrW() - cl_bHUD.Settings.map_radius - 10 - cl_bHUD.Settings.map_border, cl_bHUD.Settings.map_left, "map_left" )
 	ch = ch + 25
-	cl_bHUD.addsld( frm, "Y-Position", pw / 2 + 13, ch, 155, 10 + cl_bHUD.Settings["map_radius"] + cl_bHUD.Settings["map_border"], ScrH() - cl_bHUD.Settings["map_radius"] - 10 - cl_bHUD.Settings["map_border"], cl_bHUD.Settings["map_top"], "map_top" )
+	cl_bHUD.addsld( frm, "Y-Position", pw / 2 + 13, ch, 155, 10 + cl_bHUD.Settings.map_radius + cl_bHUD.Settings.map_border, ScrH() - cl_bHUD.Settings.map_radius - 10 - cl_bHUD.Settings.map_border, cl_bHUD.Settings.map_top, "map_top" )
 
 end
 
