f157823e2a775ba0b77ec9c057d980278df706ba
Patcher56 <patrick.kuen11@gmail.com>
2015-02-12 20:43:30 +0100

    Remove bat files

--- workshopcreator.bat
+++
@@ -1,5 +0,0 @@
-"../../../bin/gmad.exe" create -out "D:\Benutzer\Patrick\Dateien\Workshop\bhud.gma" -folder "D:\Benutzer\Patrick\Dateien\Server\Valve-Server\steamapps\common\GarrysModDS\garrysmod\addons\bHUD"
-
-"../../../bin/gmpublish" create -addon "D:\Benutzer\Patrick\Dateien\Workshop\bhud.gma" -icon "D:\Benutzer\Patrick\Pictures\Workshop\bhud.jpg"
-
-PAUSE
--- workshopupdater.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set /p changes="Changes: "
-"../../../bin/gmad.exe" create -out "D:\Daten\Workshop\bhud.gma" -folder "D:\Daten\Server\Valve Server\steamapps\common\GarrysModDS\garrysmod\addons\bHUD"
-"../../../bin/gmpublish" update -addon "D:\Daten\Workshop\bhud.gma" -id "175630527" -changes "%changes%"
-PAUSE
